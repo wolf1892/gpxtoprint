0a9897524a98e01be933baf199b151f3c7add895
Manish S <msingh@abnormal.ai>
2026-03-09 11:09:09 +0530

    fix(*):CSP errors

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: { port: 3000 },
+  server: {
+    port: 3000,
+    headers: {
+      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-eval' blob:; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: https:; connect-src 'self' https:; worker-src 'self' blob:; font-src 'self' https://cdn.jsdelivr.net https://unpkg.com data:;"
+    }
+  },
   build: { target: 'esnext' }
 });
